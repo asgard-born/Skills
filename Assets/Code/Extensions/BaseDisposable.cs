675326b56956259144159558728ff0f35a8ba43c
Viktor Kapustin <v.kapustin@ll-games.com>
2025-09-17 00:32:11 +0300

    remove addressables

--- Assets/Code/Extensions/BaseDisposable.cs
+++ Assets/Code/Extensions/BaseDisposable.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Cysharp.Threading.Tasks;
-using Extensions.Addressables;
 using Extensions.Async;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 using Object = UnityEngine.Object;
 
 namespace Extensions
@@ -69,52 +66,6 @@ namespace Extensions
                         Object.Destroy(obj);
                 }
             }
-        }
-
-        protected async UniTask<T> LoadAndTrackPrefab<T>(AssetReference reference) where T : class
-        {
-            if (reference == null)
-            {
-                Debug.LogError("reference can't be null");
-
-                return default;
-            }
-
-            (GameObject gameObject, AddressableRetain retain) = await reference.TryLoadGameObjAsync();
-
-            if (_isDisposed)
-            {
-                Debug.LogWarning($"end load addressable resourse on disposed {GetType().Name}");
-            }
-
-            string resName = reference.SubObjectName;
-
-            if (!gameObject)
-            {
-                Debug.LogError($"can't load addressable resource {resName}");
-
-                return default;
-            }
-
-            if (IsDisposed)
-            {
-                retain.Dispose();
-
-                return null;
-            }
-
-            T comp = gameObject.GetComponent<T>();
-
-            if (comp == null)
-            {
-                retain.Dispose();
-
-                return null;
-            }
-
-            AddUnsafe(retain);
-
-            return comp;
         }
 
         public TDisposable AddUnsafe<TDisposable>(TDisposable disposable) where TDisposable : IDisposable
