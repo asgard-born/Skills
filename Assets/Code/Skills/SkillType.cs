a51e4032d167c4a1c1ab6dddcfd563eeddd67f34
Viktor Kapustin <v.kapustin@ll-games.com>
2025-09-16 21:55:12 +0300

    setting up views, linked skills in view, +enter point

--- Assets/Code/Skills/SkillType.cs
+++ Assets/Code/Skills/SkillType.cs
@@ -2,19 +2,19 @@
 {
     public enum SkillType : byte
     {
-        None = 0,
-        Blade = 1,
-        Strike = 2,
-        Leap = 3,
-        Whirl = 4,
-        Guard = 5,
-        Riposte = 6,
-        Fury = 7,
-        Crescent = 8,
-        Dash = 9,
-        Lunge = 10,
-        Focus = 11,
-        Pierce = 12,
-        Slice = 13
+        None = 0,      
+        Blade = 1,     // Клинок
+        Strike = 2,    // Сильный Удар
+        Leap = 3,      // Удар в прыжке
+        Whirl = 4,     // Вертушка
+        Block = 5,     // Блок
+        Riposte = 6,   // Контратака
+        Fury = 7,      // Ярость
+        Crescent = 8,  // Удар Полумесяц
+        Dash = 9,      // Рывок
+        Lunge = 10,    // Выпад
+        Focus = 11,    // Фокус
+        Pierce = 12,   // Пронзание
+        Slice = 13     // Разрез
     }
 }
--- Assets/Code/Skills/SkillView.cs
+++ Assets/Code/Skills/SkillView.cs
@@ -1,10 +1,14 @@
-
 using UnityEngine;
 
 namespace Skills
 {
     public class SkillView : MonoBehaviour
     {
-    
+        [SerializeField] private SkillType _skillType;
+        [SerializeField] private SkillType[] _neighbors;
+
+        private int _cost;
+        private string _name;
+        private bool _isBase;
     }
 }
